127dddfd5748f2ef933464a70d03815a0ef5c44f
habla2019 pasta <kukreja.vikramaditya+habla2019@gmail.com>
2021-04-04 14:11:17 +0530

    Add sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+Sentry.init({
+  dsn: 'https://ee4884a3a50c40f086e77db49a56849b@o564398.ingest.sentry.io/5705099',
+  integrations: [ new Integrations.BrowserTracing() ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(<Router>
   <App />
